f8b0005f0320ea42cd7872cffc521f02e9913153
naitmag <me.yarmolovich@gmail.com>
2024-07-22 15:35:55 +0300

    The main app is ready. Pages index and about

--- main/views.py
+++ main/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 
-def index(request: HttpRequest) -> HttpRequest:
+def index(request: HttpRequest) -> HttpResponse:
     context = {
-
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME'
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request: HttpRequest) -> HttpRequest:
-    return HttpResponse('about')
+    context = {
+        'title': 'Home - О нас',
+        'content': 'О нас',
+        'text_on_page': 'Это текст про наш магазин'
+    }
+
+    return render(request, 'main/about.html', context)
