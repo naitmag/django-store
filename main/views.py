439ac243666fcc2a189bf37ffd288e2a488397e8
naitmag <me.yarmolovich@gmail.com>
2024-08-01 16:54:00 +0300

    images for goods added changed catalog page and serach placeholder in base.html

--- main/views.py
+++ main/views.py
@@ -1,16 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
-from goods.models import Categories
+from goods.models import Categories, Products
+
 
 def index(request: HttpRequest) -> HttpResponse:
-
     categories = Categories.objects.all()
+    goods = Products.objects.all()
 
     context = {
         'title': 'Home - Главная',
         'content': 'Магазин мебели HOME',
-        'categories' : categories
+        'categories': categories,
+        'goods': goods
     }
 
     return render(request, 'main/index.html', context)
