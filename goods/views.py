799bbfed762e921675ac006c7dc0ddb9d57bd90f
naitmag <me.yarmolovich@gmail.com>
2024-08-09 17:43:32 +0300

    Fix product filter site view and added urlencode

--- goods/views.py
+++ goods/views.py
@@ -1,10 +1,10 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_list_or_404, get_object_or_404
 from goods.models import Products
 
 
 def catalog(request, category_slug):
-    print(request.GET)
+
     page = request.GET.get('page', 1)
     on_sale = request.GET.get('on_sale', None)
     order_by = request.GET.get('order_by', None)
@@ -20,7 +20,7 @@ def catalog(request, category_slug):
     if order_by and order_by != 'default':
         goods = goods.order_by(order_by)
 
-    paginator = Paginator(goods, 6)
+    paginator = Paginator(goods, 3)
     current_page = paginator.page(int(page))
 
     context = {
