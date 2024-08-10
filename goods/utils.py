4e57ad018150173fd52b3764c0c0fabc7976504f
naitmag <me.yarmolovich@gmail.com>
2024-08-10 12:47:43 +0300

    Improved catalog search

--- goods/utils.py
+++ goods/utils.py
@@ -1,11 +1,19 @@
 from django.shortcuts import get_list_or_404
+from django.db.models import Q
 
 from goods.models import Products
 
 
 def query_search(query: str):
-    if query.isdigit() and len(query) <5:
+    if query.isdigit() and len(query) < 5:
         return get_list_or_404(Products.objects.filter(id=int(query)))
 
-    return get_list_or_404(Products.objects.filter(name__icontains=query))
+    keywords = [word for word in query.split() if len(word) > 2]
 
+    q_objects = Q()
+
+    for token in keywords:
+        q_objects |= Q(description__icontains=token)
+        q_objects |= Q(name__icontains=token)
+
+    return Products.objects.filter(q_objects)
