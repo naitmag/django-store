91c4f873e11f8f38d50bbb7e4a99da5e1ca579f8
naitmag <me.yarmolovich@gmail.com>
2024-08-13 20:12:49 +0300

    Fulltext search upgrade

--- goods/utils.py
+++ goods/utils.py
@@ -1,20 +1,34 @@
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 from django.shortcuts import get_list_or_404
-from django.db.models import Q
 
 from goods.models import Products
 
 
 def query_search(query: str):
     if query.isdigit() and len(query) < 5:
-        return get_list_or_404(Products.objects.filter(id=int(query)))
+        return Products.objects.filter(id=int(query))
 
     vector = SearchVector('name', 'description')
     query = SearchQuery(query)
-    return get_list_or_404(
-        Products.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = Products.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel='</span>'),
+    )
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: yellow;">',
+            stop_sel='</span>'),
+
     )
 
+    return result
     # keywords = [word for word in query.split() if len(word) > 2]
     #
     # q_objects = Q()
