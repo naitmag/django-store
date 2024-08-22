9038ea866d34a2940229a9c984fa168c57ef48f9
naitmag <me.yarmolovich@gmail.com>
2024-08-22 14:40:55 +0300

    Registration form and controller

--- users/views.py
+++ users/views.py
@@ -1,9 +1,9 @@
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 
 def login(request):
@@ -14,21 +14,32 @@ def login(request):
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
             if user:
-                auth.login(request,user)
+                auth.login(request, user)
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
 
     context = {
-            'title': 'Home - Авторизация',
-            'form': form
-        }
+        'title': 'Home - Авторизация',
+        'form': form
+    }
     return render(request, 'users/login.html', context)
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserRegistrationForm()
+
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form,
     }
     return render(request, 'users/registration.html', context)
 
@@ -41,4 +52,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
