45eaf800cad0b4a83745d2e62c176def288a1cd3
naitmag <me.yarmolovich@gmail.com>
2024-08-22 13:59:08 +0300

    Login form and controller final version

--- users/forms.py
+++ users/forms.py
@@ -5,27 +5,8 @@ from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
-    username = forms.CharField(
-        label="Имя пользователя:",
-        widget=forms.TextInput(
-            attrs={
-                "autofocus": True,
-                "class": "form-control",
-                "placeholder": "Введите имя пользователя"
-            }
-        )
-    )
-    password = forms.CharField(
-        label="Пароль:",
-        strip=False,
-        widget=forms.PasswordInput(
-            attrs={
-                "autocomplete": "current-password",
-                "class": "form-control",
-                "placeholder": "Введите пароль"
-            }
-        )
-    )
+    username = forms.CharField()
+    password = forms.CharField()
 
     class Meta:
         model = User
